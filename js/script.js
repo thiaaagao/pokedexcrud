e69266c7881be8db5aacd2ee71b33486e2073647
thiaaagao <rdes.thiago@gmail.com>
2023-05-19 22:08:59 -0300

    add: fix bug table and add table update

--- js/script.js
+++ js/script.js
@@ -1,53 +1,96 @@
-
-function cadastrarPokemon(){
-    pokeTemp.pokemon = document.getElementById("inputId").value;
-    pokeTemp.tipo = document.getElementById("inputType").value;
-
-    if (!pokeTemp.pokemon || !pokeTemp.tipo) {
-        alert('Preencha todos os campos!')
-        return;
-    }
-    createPokemon(pokeTemp);
+// id = formPokemon
+const isValidInputId = () => {
+    return document.getElementById("formPokemon").reportValidity();
 }
 
 
+// id="pokemonName"
+// id="pokemonType"
+const clearInputs = () => {
+    const inputs = document.querySelectorAll('.inputPokemon') // reset inputs blank
+    inputs.forEach(input => input.value = '')
+}
 
-// var inputs = $('input').on('keyup', verificarInputs);
+const cadastrarPokemon = () => {
+    if (isValidInputId()) {
+        const pokemon = {
+            nomePokemon: document.getElementById("pokemonName").value,
+            tipo: document.getElementById("pokemonType").value
+        }
+        createPokemon(pokemon)
+        clearInputs()
+        updateTable()
+    }
+}
 
-// function verificarInputs() {
-//     const preenchidos = inputs.get().every(({value}) => value)
-//     $('button').prop('disabled', !preenchidos);
-// }
 
-
+/* ESSA MERDA VAI SAIR ! */
 const pokeTemp = {
-    pokemon: 'dragonite',
-    tipo: 'fogo'
+    nomePokemon: 'dragonite',
+    tipo: 'dragao'
 }
 
 // FUNCAO SET AND GET DB
-const getLocalStorage = () => JSON.parse(localStorage.getItem('db_pokemon')) ?? []
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_pokemon')) ?? [] // parse in JSON STRING
 const setLocalStorage = (dbPokemon) => localStorage.setItem('db_pokemon', JSON.stringify(dbPokemon))
 
-// C - CREATE POKEMON
+// C - CREATE POKEMON DB
 const createPokemon = (pokemon) => {
     const dbPokemon = getLocalStorage()
-    dbPokemon.push (pokemon)
+    dbPokemon.push(pokemon)
     setLocalStorage(dbPokemon)
 }
-// R - READ POKE
+
+// R - READ POKE DB
 const readPoke = () => getLocalStorage()
 
-// U - UPDATE
+// U - UPDATE DB
 const updatePoke = (index, pokemon) => {
     const dbPokemon = readPoke()
     dbPokemon[index] = pokemon;
     setLocalStorage(dbPokemon)
 }
 
-// D - DELETE
+// D - DELETE DB
 const deletePoke = (index) => {
     const dbPokemon = readPoke()
-    dbPokemon.slice(index, 1)
+    dbPokemon.splice(index, 1)
     setLocalStorage(dbPokemon)
 }
+
+// create row
+const createRow = (pokemon, index) => {
+    const newRow = document.createElement('tr')
+    newRow.innerHTML = `
+        <td style="border: 1px solid #000">${index+1}</td>
+        <td style="border: 1px solid #000">${pokemon.nomePokemon}</td>
+        <td style="border: 1px solid #000">${pokemon.tipo}</td>
+        <td style="border: 1px solid #000"><input type="button" value="Editar Pokemon"> - <input
+        type="button" value="Deletar Pokemon"></td>
+    `
+    document.querySelector('#tablePokemon>tbody').appendChild(newRow)
+    // id="tablePokemon"
+}
+
+// clear table
+const clearTable = () => {
+    const rows = document.querySelectorAll('#tablePokemon>tbody tr');
+    rows.forEach(row => row.parentNode.removeChild(row))
+}
+
+// update table
+const updateTable = () => {
+    const dbPokemon = readPoke()
+    clearTable()
+    dbPokemon.forEach(createRow)
+}
+
+updateTable()
+
+// Events
+// document.querySelector('#tablePokemon>tbody').addEventListener('click', deletePoke)
+
+//anotações-Adam
+//data-action="delete"
+//data-action="edit-${index}"
+//event.target.dataset.actions.delete
